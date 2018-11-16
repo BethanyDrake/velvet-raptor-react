f60aebd10aa8e83aa34eee02a1cc2a7f5eaa3670
Bethany Drake <bethanyd@thoughtworks.com>
2018-11-16 11:32:20 +1100

    attempt to fix circleci configh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          Hi! AAAAA
+          Hi! BBBBB
         </header>
       </div>
     );
